a80e0283112a50a89782f0db189e05bbf33f74e1
juanHernandezG <juan.hernandez1901@alumnos.biobio.cl>
2023-07-11 14:07:14 -0400

    configuraciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PanelDeControlComponent } from './pages/panel-de-control/panel-de-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelDeControlComponent
   ],
   imports: [
     BrowserModule,
