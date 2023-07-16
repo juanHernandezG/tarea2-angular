4b0298461ab84737c381a4e80461d0ec858663f5
juanHernandezG <juan.hernandez1901@alumnos.biobio.cl>
2023-07-16 17:20:04 -0400

    marcador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MarcadorComponent } from './pages/marcador/marcador.component';
+import { PanelDeControlComponent } from './pages/panel-de-control/panel-de-control.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'marcador', component: MarcadorComponent},
+  {path: 'panel-control', component: PanelDeControlComponent},
+  {path: '', redirectTo:'marcador', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
