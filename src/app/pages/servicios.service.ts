7c162b0eba52f35a6439b3a1c6d0d059102e5be5
Joaquin Poblete <joaquinlolvlad@gmail.com>
2023-07-19 22:04:21 -0400

    boton faltas

--- src/app/pages/servicios.service.ts
+++ src/app/pages/servicios.service.ts
@@ -5,12 +5,18 @@ import { BehaviorSubject, Subscription, interval } from 'rxjs';
   providedIn: 'root'
 })
 export class ServiciosService {
-  
+
   private puntuacionLocalSubject = new BehaviorSubject<number>(0);
   private puntuacionVisitaSubject = new BehaviorSubject<number>(0);
 
+  private faltasLocalSubject = new BehaviorSubject<number>(0);
+  private faltasVisitaSubject = new BehaviorSubject<number>(0);
+
   puntuacionLocal$ = this.puntuacionLocalSubject.asObservable();
   puntuacionVisita$ = this.puntuacionVisitaSubject.asObservable();
+
+  faltasLocal$ = this.faltasLocalSubject.asObservable();
+  faltasVisita$ = this.faltasVisitaSubject.asObservable();
 
   aumentarPuntuacionLocal(puntos: number) {
     this.puntuacionLocalSubject.next(this.puntuacionLocalSubject.value + puntos);
@@ -18,5 +24,13 @@ export class ServiciosService {
 
   aumentarPuntuacionVisita(puntos: number) {
     this.puntuacionVisitaSubject.next(this.puntuacionVisitaSubject.value + puntos);
+  }
+
+  aumentarFaltaLocal(puntos: number){
+    this.faltasLocalSubject.next(this.faltasLocalSubject.value + puntos);
+  }
+
+  aumentarFaltaVisita(puntos: number){
+    this.faltasVisitaSubject.next(this.faltasVisitaSubject.value + puntos);
   }
 }
